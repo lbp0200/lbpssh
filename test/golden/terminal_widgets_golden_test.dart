4ac4e797a402acfa596d79c928c374d91b40cb94
liuboping <liuboping@elex-tech.com>
2026-03-14 02:11:33 +0800

    fix(golden): remove unused import

--- test/golden/terminal_widgets_golden_test.dart
+++ test/golden/terminal_widgets_golden_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:golden_toolkit/golden_toolkit.dart';
 
 void main() {
   group('Golden Tests', () {
