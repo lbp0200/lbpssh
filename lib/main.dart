40d140741e932f0d49696475b0b5e6f5fbccf876
liuboping <liuboping@elex-tech.com>
2026-03-15 23:37:55 +0800

    fix: add GlobalCupertinoLocalizations delegate
    - Add GlobalCupertinoLocalizations.delegate to fix zh locale warning
    - Cupertino widgets now have proper localization support

--- lib/main.dart
+++ lib/main.dart
@@ -96,6 +96,7 @@ class MyApp extends StatelessWidget {
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: const [
         Locale('en'),
